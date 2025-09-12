c124d4ec74545f56e993f020714f276a85bee1ce
MeLikeApplez <jeremisibayan@gmail.com>
2025-09-12 13:39:52 -0500

    multiplyMatrices

--- js/Math/Matrix3.js
+++ js/Math/Matrix3.js
@@ -117,6 +117,33 @@ export default class Matrix3 extends Array {
         }
         return this;
     }
+    // https://github.com/mrdoob/three.js/blob/0af9729d0c143a86a1d725d6e2c3ad83301f3f34/src/math/Matrix3.js#L215
+    /**
+     * @param {Matrix3} a
+     * @param {Matrix3} b
+     * @returns {this}
+     */
+    multiplyMatrices(a, b) {
+        const ae = a;
+        const be = b;
+        const te = this;
+        const a11 = ae[0], a12 = ae[3], a13 = ae[6];
+        const a21 = ae[1], a22 = ae[4], a23 = ae[7];
+        const a31 = ae[2], a32 = ae[5], a33 = ae[8];
+        const b11 = be[0], b12 = be[3], b13 = be[6];
+        const b21 = be[1], b22 = be[4], b23 = be[7];
+        const b31 = be[2], b32 = be[5], b33 = be[8];
+        te[0] = a11 * b11 + a12 * b21 + a13 * b31;
+        te[3] = a11 * b12 + a12 * b22 + a13 * b32;
+        te[6] = a11 * b13 + a12 * b23 + a13 * b33;
+        te[1] = a21 * b11 + a22 * b21 + a23 * b31;
+        te[4] = a21 * b12 + a22 * b22 + a23 * b32;
+        te[7] = a21 * b13 + a22 * b23 + a23 * b33;
+        te[2] = a31 * b11 + a32 * b21 + a33 * b31;
+        te[5] = a31 * b12 + a32 * b22 + a33 * b32;
+        te[8] = a31 * b13 + a32 * b23 + a33 * b33;
+        return this;
+    }
     /**
      * @param {number} scale
      * @returns {this}
@@ -146,12 +173,13 @@ export default class Matrix3 extends Array {
      * @param {Vector2} position
      * @param {number} theta
      * @param {Vector2} scale
-     * @returns {void}
+     * @returns {this}
      */
     compose(position, theta, scale) {
         const sin = Math.sin(theta);
         const cos = Math.cos(theta);
         this.set(cos * scale.x, -sin * scale.y, position.x, sin * scale.x, cos * scale.y, position.y, 0, 0, 1);
+        return this;
     }
     /**
      * @returns {Matrix3}
